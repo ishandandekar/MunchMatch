16a2be5b41d7c1ec10da0460987c8591dbfe6594
Ishan Dandekar <ishandandekar2002@gmail.com>
2022-07-03 22:41:44 +0530

    Add app script

--- README.md
+++ README.md
@@ -4,14 +4,17 @@
 </p>
 
 
-Hello and welcome, What_Am_I_Eating is my first computer vision project (get the meme now?).  
-Check out the [Medium](https://medium.com/@ishandandekar/foodvision-3843f38be45e) article as a supplement to this README. The article describes the project better.
+Hello and welcome to What_Am_I_Eating. What_Am_I_Eating is my first computer vision project (get the meme now? the meme has vision in it and vision is also a computer, so "computer vision").
+Check out the [Medium](https://medium.com/@ishandandekar/foodvision-3843f38be45e) article as a supplement to this README. The article describes the project better.  
 
-## About
-What_Am_I_Eating is an app which classifies the food item present in the image.  The main aim of building this model was to beat the [DeepFood](https://arxiv.org/abs/1606.05675)📄 paper.The app uses a neural network to classify these images into 101 categories of food items.
+## Introduction
+What_Am_I_Eating is an app which classifies the food item present in the image.  The main aim of building this model was to beat the [DeepFood](https://arxiv.org/abs/1606.05675)📄 paper.The app uses a neural network to classify these images into 101 categories of food items. I made this project while completing the [Zero to mastery Tensorflow course](https://zerotomastery.io/courses/learn-tensorflow/).  
+
+While doing this project I learned the various techniques and processes. I learned how to get preprocessed data using `tensorflow-datasets` library,how we can leverage other pre-trained models using **transfer learning**, how we can fine-tune these pre-trained models using *fine tuning* method.
 
 ## Data
-Data used to train the image is available on [Kaggle](https://www.kaggle.com/datasets/dansbecker/food-101). Although I used the dataset available in the [tensorflow datasets catalog](https://www.tensorflow.org/datasets/catalog/food101). The advantage of using the `tensorflow_datasets` module is that the data is already in tensors. Hence, there is little to no requirement of preprocessing.
+The model is trained on the **[Food101](https://data.vision.ee.ethz.ch/cvl/datasets_extra/food-101/)** dataset. This dataset was used in the various papers such as [DeepFood](https://arxiv.org/abs/1606.05675) and [Food-101](https://data.vision.ee.ethz.ch/cvl/datasets_extra/food-101/). The data has 101 food categories and a total of 101,000 images. For each class, there are 250 test images and 750 train images. This is a lot of data. To optimize training I used the Food101 data available in *tensorflow-datasets* library. This made it really easy to use, as the data was already in tensors.
+> :bulb:**Note:** You can also find the dataset on **Kaggle** ([link](https://www.kaggle.com/datasets/dansbecker/food-101))
 
 ## Model
 The model uses transfer learning to use the `EfficientNetB0` architecture under the hood. The model has four layers, namely:
@@ -19,8 +22,15 @@ The model uses transfer learning to use the `EfficientNetB0` architecture under 
 * EfficientNetB0: Using the keras API and exploiting the transfer learning, the neural network uses a `EfficientNetB0`. This is a pretrained model on the classical ImageNet dataset. We fine-tune the weights and biases to make the predictions better. Learn more about the architecture of `EfficientNetB0` - [architecture](https://ai.googleblog.com/2019/05/efficientnet-improving-accuracy-and.html)
 * GlobalAveragePooling2D layer: This layer takes the average of all the numbers in the previous layer and then condenses it into a (1,3) tensor. Layers such as GlobalAveragePooling2D layer, MaxPooling layers etc., usually come in handy in CNNs as there are a lot of numbers and the output layer may take much time to then predict classes.
 * Dense layer: This is used so that we have 1 neuron for each class.
-* Activation layer: This layer is used to finally classify the tensors in classes. We have used `softmax` activation function as this is a multi-class classification problem. The activation could've have been integrated with the `Dense` layer itself. We have used this type of structure to use the `mixed_precision` feature of tensorflow and keras.  
-We then compile the model with `sparse_categorical_crossentropy` loss, `Adam` optimizer and use `accuracy` as a metric.
+* Activation layer: This layer is used to finally classify the tensors in classes. We have used `softmax` activation function as this is a multi-class classification problem. The activation could've have been integrated with the `Dense` layer itself. We have used this type of structure to use the `mixed_precision` feature of tensorflow and keras.
+We then compile the model with `sparse_categorical_crossentropy` loss, `Adam` optimizer and use `accuracy` as a metric.  
+
+The model got around **78% accuracy** on the test set.
 
 ## App
 The web app is made using Streamlit. I chose this framework as it is very easy to make web apps with it and then deploy it. The website is accompanied by the model (in .h5 file format). This reduces the time taken to predict. The website also displays a probability graph after the prediction. This graph shows the probability of the image being of that class (only top 5 classes).
+
+## Contents of the repository
+* assets: This folder contains images and other unecessary files, which are either required by the app or the Medium article
+* fine_tuned_model.h5: This is the Tensorflow model (in .h5 format) which the app uses to classify the images
+* app.py: The web app made using streamlit library. This is the python script use to make the web app.
---
+++ app.py
@@ -0,0 +1,178 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.set_page_config(page_title="What_Am_I_Eating",page_icon=":eyes:",layout="wide")
+st.markdown("""
+        <style>
+               .css-18e3th9 {
+                    padding-top: 0rem;
+                    padding-bottom: 10rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+               .css-1d391kg {
+                    padding-top: 3.5rem;
+                    padding-right: 1rem;
+                    padding-bottom: 3.5rem;
+                    padding-left: 1rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+
+CLASS_NAMES=['apple_pie',
+ 'baby_back_ribs',
+ 'baklava',
+ 'beef_carpaccio',
+ 'beef_tartare',
+ 'beet_salad',
+ 'beignets',
+ 'bibimbap',
+ 'bread_pudding',
+ 'breakfast_burrito',
+ 'bruschetta',
+ 'caesar_salad',
+ 'cannoli',
+ 'caprese_salad',
+ 'carrot_cake',
+ 'ceviche',
+ 'cheese_plate',
+ 'cheesecake',
+ 'chicken_curry',
+ 'chicken_quesadilla',
+ 'chicken_wings',
+ 'chocolate_cake',
+ 'chocolate_mousse',
+ 'churros',
+ 'clam_chowder',
+ 'club_sandwich',
+ 'crab_cakes',
+ 'creme_brulee',
+ 'croque_madame',
+ 'cup_cakes',
+ 'deviled_eggs',
+ 'donuts',
+ 'dumplings',
+ 'edamame',
+ 'eggs_benedict',
+ 'escargots',
+ 'falafel',
+ 'filet_mignon',
+ 'fish_and_chips',
+ 'foie_gras',
+ 'french_fries',
+ 'french_onion_soup',
+ 'french_toast',
+ 'fried_calamari',
+ 'fried_rice',
+ 'frozen_yogurt',
+ 'garlic_bread',
+ 'gnocchi',
+ 'greek_salad',
+ 'grilled_cheese_sandwich',
+ 'grilled_salmon',
+ 'guacamole',
+ 'gyoza',
+ 'hamburger',
+ 'hot_and_sour_soup',
+ 'hot_dog',
+ 'huevos_rancheros',
+ 'hummus',
+ 'ice_cream',
+ 'lasagna',
+ 'lobster_bisque',
+ 'lobster_roll_sandwich',
+ 'macaroni_and_cheese',
+ 'macarons',
+ 'miso_soup',
+ 'mussels',
+ 'nachos',
+ 'omelette',
+ 'onion_rings',
+ 'oysters',
+ 'pad_thai',
+ 'paella',
+ 'pancakes',
+ 'panna_cotta',
+ 'peking_duck',
+ 'pho',
+ 'pizza',
+ 'pork_chop',
+ 'poutine',
+ 'prime_rib',
+ 'pulled_pork_sandwich',
+ 'ramen',
+ 'ravioli',
+ 'red_velvet_cake',
+ 'risotto',
+ 'samosa',
+ 'sashimi',
+ 'scallops',
+ 'seaweed_salad',
+ 'shrimp_and_grits',
+ 'spaghetti_bolognese',
+ 'spaghetti_carbonara',
+ 'spring_rolls',
+ 'steak',
+ 'strawberry_shortcake',
+ 'sushi',
+ 'tacos',
+ 'takoyaki',
+ 'tiramisu',
+ 'tuna_tartare',
+ 'waffles']
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+	model = tf.keras.models.load_model('fine_tuned_model.h5')
+	return model
+
+def load_and_prep(filename,img_shape=224):
+    img = tf.cast(filename,tf.float32)
+    img = tf.image.resize(img, [img_shape, img_shape])
+    return img
+
+def predict(image,model):
+    image = load_and_prep(image)
+    pred_prob = model.predict(tf.expand_dims(image, axis=0),verbose=0) # make prediction on image with shape [None, 224, 224, 3]
+    pred_class = CLASS_NAMES[pred_prob.argmax()]
+    st.success(f"The model predicts, it is a {pred_class.capitalize()}")
+
+st.markdown("<h1 style='text-align: center;'>What_Am_I_Eating &#127828&#128064;</h1>", unsafe_allow_html=True)
+col1,col2 = st.columns([2,1])
+
+with col1:
+    st.markdown(":wave: Hello and welcome to the **What_Am_I_Eating** web app. **What_Am_I_Eating** is a web app which categorises food images. It uses a model made using **Tensorflow** (Google's open source library).")
+    st.markdown("""
+### Introduction
+What_Am_I_Eating is an app which classifies the food item present in the image.  The main aim of building this model was to beat the [DeepFood](https://arxiv.org/abs/1606.05675)📄 paper.The app uses a **neural network** made using Tensorflow to classify these images into 101 categories of food items. I made this project while completing the [Zero to mastery Tensorflow course](https://zerotomastery.io/courses/learn-tensorflow/).
+
+### Data
+The model is trained on the **[Food101](https://data.vision.ee.ethz.ch/cvl/datasets_extra/food-101/)** dataset. This dataset was used in the various papers such as [DeepFood](https://arxiv.org/abs/1606.05675) and [Food-101](https://data.vision.ee.ethz.ch/cvl/datasets_extra/food-101/). The data has 101 food categories and a total of 101,000 images. For each class, there are 250 test images and 750 train images. This is a lot of data. To optimize training I used the Food101 data available in *tensorflow-datasets* library. This made it really easy to use, as the data was already in tensors.
+
+### Model
+As said, the model is a neural network made using Tensorflow. The neural network uses *EfficientNetB0* as its backbone. The model has four layers, namely:
+* The Input layer: Confirms the inputs to the neural network is in the form of tensor with shape (224,224,3).
+* EfficientNetB0: Using the keras API and exploiting the transfer learning, the neural network uses a *EfficientNetB0*. This is a pretrained model on the classical ImageNet dataset. We fine-tune the weights and biases to make the predictions better. Learn more about the architecture of *EfficientNetB0* - [architecture](https://ai.googleblog.com/2019/05/efficientnet-improving-accuracy-and.html)
+* GlobalAveragePooling2D layer: This layer takes the average of all the numbers in the previous layer and then condenses it into a (1,3) tensor. Layers such as GlobalAveragePooling2D layer, MaxPooling layers etc., usually come in handy in CNNs as there are a lot of numbers and the output layer may take much time to then predict classes.
+* Dense layer: This is used so that we have 1 neuron for each class.
+* Activation layer: This layer is used to finally classify the tensors in classes. We have used *softmax* activation function as this is a multi-class classification problem. The activation could've have been integrated with the *Dense* layer itself. We have used this type of structure to use the *mixed_precision* feature of tensorflow and keras.
+We then compile the model with *sparse_categorical_crossentropy* loss, *Adam* optimizer and use *accuracy* as a metric.
+""",unsafe_allow_html=True)
+
+with col2:
+    st.markdown("""
+    ### Predict on your image!
+    """)
+    if st.checkbox("Show labels"):
+        st.write("There are 101 classes, so these are some 5 labels")
+        import random
+        st.write(random.sample(CLASS_NAMES,5))
+    image = st.file_uploader(label="Choose and image",type=['png','jpg','jpeg'])
+    if image is not None:
+        st.image(image=image)
+        test_image = Image.open(image)
+        model = load_model()
+        if st.button("Predict"):
+            predict(test_image,model)
