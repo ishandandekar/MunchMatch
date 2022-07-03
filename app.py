8ea788ce8b469d771439315a24c699416042471e
Ishan Dandekar <ishandandekar2002@gmail.com>
2022-07-03 23:34:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def predict(image,model):
     image = load_and_prep(image)
     pred_prob = model.predict(tf.expand_dims(image, axis=0),verbose=0) # make prediction on image with shape [None, 224, 224, 3]
     pred_class = CLASS_NAMES[pred_prob.argmax()]
-    st.success(f"The model predicts, it is a {pred_class.capitalize()}")
+    st.success(f"The model predicts, it is a {pred_class}")
 
 st.markdown("<h1 style='text-align: center;'>What_Am_I_Eating &#127828&#128064;</h1>", unsafe_allow_html=True)
 col1,col2 = st.columns([2,1])
