934300eaf686434b5eade91741c77e9134605d12
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-07-07 23:45:10 +0530

    Update documentation

--- scripts/helper_functions.py
+++ scripts/helper_functions.py
@@ -1,50 +1,55 @@
 import matplotlib.pyplot as plt
 
+
 def plot_loss_curves(history):
-  """
-  Returns separate loss curves for training and validation metrics.
-  Args:
-    history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
-  """
-  loss = history.history['loss']
-  val_loss = history.history['val_loss']
+    """
+    Returns separate loss curves for training and validation metrics.
 
-  accuracy = history.history['accuracy']
-  val_accuracy = history.history['val_accuracy']
+    Args:
+      history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
+    """
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
 
-  epochs = range(len(history.history['loss']))
+    accuracy = history.history["accuracy"]
+    val_accuracy = history.history["val_accuracy"]
 
-  # Plot loss
-  plt.plot(epochs, loss, label='training_loss')
-  plt.plot(epochs, val_loss, label='val_loss')
-  plt.title('Loss')
-  plt.xlabel('Epochs')
-  plt.legend()
+    epochs = range(len(history.history["loss"]))
 
-  # Plot accuracy
-  plt.figure()
-  plt.plot(epochs, accuracy, label='training_accuracy')
-  plt.plot(epochs, val_accuracy, label='val_accuracy')
-  plt.title('Accuracy')
-  plt.xlabel('Epochs')
-  plt.legend();
+    # Plot loss
+    plt.plot(epochs, loss, label="training_loss")
+    plt.plot(epochs, val_loss, label="val_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
 
-def show_random_samples(dir_name,class_names):
-  """
-  Shows n random samples from directory
+    # Plot accuracy
+    plt.figure()
+    plt.plot(epochs, accuracy, label="training_accuracy")
+    plt.plot(epochs, val_accuracy, label="val_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
 
-  Args:
-    dir_name: Directory to look into while getting images
-    n_samples: Number of samples to show
-    class_names: List of class names of the image data
-  """
-  for image, label in dir_name.take(1):
-    print(f"""
+
+def show_random_samples(dir_name, class_names):
+    """
+    Shows n random samples from directory
+
+    Args:
+      dir_name: Directory to look into while getting images
+      n_samples: Number of samples to show
+      class_names: List of class names of the image data
+    """
+    for image, label in dir_name.take(1):
+        print(
+            f"""
     Image shape: {image.shape},
     Image datatype: {image.dtype},
     Target class from Food101 (tensor form): {label},
     Class name: {class_names[label.numpy()]}
-    """)
-    plt.imshow(image/255)
-    plt.title(f"{class_names[label.numpy()]}")
-    plt.axis(False)
+    """
+        )
+        plt.imshow(image / 255)
+        plt.title(f"{class_names[label.numpy()]}")
+        plt.axis(False)
